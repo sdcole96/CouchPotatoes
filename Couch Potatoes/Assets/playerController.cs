b4bec11d02f2433c65230d5d62ed3404dac22070
Daniel <sdcole96@yahoo.com>
2017-10-04 13:52:43 -0700

    CharacterMovement Updated
    Character currently looks in direction of joystick, but resets to
    original position when you stop moving.

---
+++ Couch Potatoes/Assets/playerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	public float speed = 3.0f;
+	public float jumpForce = 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+		Debug.Log ("Horizontal: " + x);
+		Debug.Log ("Horizontal: " + z);
+
+		transform.Rotate(0, 0, 0);
+		transform.Translate(x, 0, z);
+
+		if(Input.GetKeyUp(KeyCode.Space))
+		{ 
+			jump();
+		} 
+	}
+
+	public void jump()
+	{
+		
+	}
+}
---
+++ Couch Potatoes/Assets/rotationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var x = Input.GetAxisRaw("Horizontal") ;
+		var z = Input.GetAxisRaw("Vertical") ;
+		float heading = Mathf.Atan2(x,z);
+		Debug.Log ("DEGREE: "+heading * Mathf.Rad2Deg);
+		transform.rotation = Quaternion.Euler (0f, heading * Mathf.Rad2Deg, 0f);
+		transform.Rotate(0, 0, 0);
+	}
+}
