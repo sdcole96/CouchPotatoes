6049414706becce3a13ae3b2d330ee5a2c412b49
Julien Fournell <fourn106@mail.chapman.edu>
2017-10-18 21:51:18 -0700

    Add files via upload

--- Couch Potatoes/Assets/Our Assets/Scripts/XboxController.cs
+++ Couch Potatoes/Assets/Our Assets/Scripts/XboxController.cs
@@ -6,108 +6,134 @@ public class XboxController
 {
 
 	public int playerNum = -1;
+    public bool isMac = false;
 
 	public bool getAButton(ButtonQuery q)
 	{
-		string input = "JoystickBtnA";
-		switch (q) 
-		{
-			case ButtonQuery.Up:
-				return Input.GetButtonUp (input + playerNum);
-			case ButtonQuery.Down:
-				return Input.GetButtonDown(input + playerNum);
-			case ButtonQuery.Hold:
-				return Input.GetButton(input + playerNum);
-		}
-		return false;
-	}
+        string input = "JoystickBtnA";
+        if (isMac)
+        {
+            input = input + "Mac";
+        }
+        switch (q)
+        {
+            case ButtonQuery.Up:
+                return Input.GetButtonUp(input + playerNum);
+            case ButtonQuery.Down:
+                return Input.GetButtonDown(input + playerNum);
+            case ButtonQuery.Hold:
+                return Input.GetButton(input + playerNum);
+        }
+        return false;
+    }
 
 	public bool getBButton(ButtonQuery q)
 	{
-		string input = "JoystickBtnB";
-		switch (q) 
-		{
-		case ButtonQuery.Up:
-			return Input.GetButtonUp (input + playerNum);
-		case ButtonQuery.Down:
-			return Input.GetButtonDown(input +playerNum);
-		case ButtonQuery.Hold:
-			return Input.GetButton(input +playerNum);
-		}
-		return false;
-	}
+        string input = "JoystickBtnA";
+        if (isMac)
+        {
+            input = input + "Mac";
+        }
+        switch (q)
+        {
+            case ButtonQuery.Up:
+                return Input.GetButtonUp(input + playerNum);
+            case ButtonQuery.Down:
+                return Input.GetButtonDown(input + playerNum);
+            case ButtonQuery.Hold:
+                return Input.GetButton(input + playerNum);
+        }
+        return false;
+    }
 
 	public bool getYButton(ButtonQuery q)
 	{
-		string input = "JoystickBtnY";
-		switch (q) 
-		{
-		case ButtonQuery.Up:
-			return Input.GetButtonUp (input + playerNum);
-		case ButtonQuery.Down:
-			return Input.GetButtonDown(input +playerNum);
-		case ButtonQuery.Hold:
-			return Input.GetButton(input +playerNum);
-		}
-		return false;
-	}
+        string input = "JoystickBtnA";
+        if (isMac)
+        {
+            input = input + "Mac";
+        }
+        switch (q)
+        {
+            case ButtonQuery.Up:
+                return Input.GetButtonUp(input + playerNum);
+            case ButtonQuery.Down:
+                return Input.GetButtonDown(input + playerNum);
+            case ButtonQuery.Hold:
+                return Input.GetButton(input + playerNum);
+        }
+        return false;
+    }
 
 	public bool getXButton(ButtonQuery q)
 	{
-		string input = "JoystickBtnX";
-		switch (q) 
-		{
-		case ButtonQuery.Up:
-			return Input.GetButtonUp (input + playerNum);
-		case ButtonQuery.Down:
-			return Input.GetButtonDown(input +playerNum);
-		case ButtonQuery.Hold:
-			return Input.GetButton(input +playerNum);
-		}
-		return false;
-	}
+        string input = "JoystickBtnA";
+        if (isMac)
+        {
+            input = input + "Mac";
+        }
+        switch (q)
+        {
+            case ButtonQuery.Up:
+                return Input.GetButtonUp(input + playerNum);
+            case ButtonQuery.Down:
+                return Input.GetButtonDown(input + playerNum);
+            case ButtonQuery.Hold:
+                return Input.GetButton(input + playerNum);
+        }
+        return false;
+    }
 
 	public bool getRightTrigger(ButtonQuery q)
 	{
-		string input = "JoystickRightTrigger";
-		switch (q) 
-		{
-		case ButtonQuery.Up:
-			return Input.GetButtonUp (input + playerNum);
-		case ButtonQuery.Down:
-			return Input.GetButtonDown(input +playerNum);
-		case ButtonQuery.Hold:
-			return Input.GetButton(input +playerNum);
-		}
-		return false;
-	}
+        string input = "JoystickBtnA";
+        if (isMac)
+        {
+            input = input + "Mac";
+        }
+        switch (q)
+        {
+            case ButtonQuery.Up:
+                return Input.GetButtonUp(input + playerNum);
+            case ButtonQuery.Down:
+                return Input.GetButtonDown(input + playerNum);
+            case ButtonQuery.Hold:
+                return Input.GetButton(input + playerNum);
+        }
+        return false;
+    }
 
 	public bool getLeftTrigger(ButtonQuery q)
 	{
-		string input = "JoystickLeftTrigger";
-		switch (q) 
-		{
-		case ButtonQuery.Up:
-			return Input.GetButtonUp (input + playerNum);
-		case ButtonQuery.Down:
-			return Input.GetButtonDown(input +playerNum);
-		case ButtonQuery.Hold:
-			return Input.GetButton(input +playerNum);
-		}
-		return false;
-	}
+        string input = "JoystickBtnA";
+        if (isMac)
+        {
+            input = input + "Mac";
+        }
+        switch (q)
+        {
+            case ButtonQuery.Up:
+                return Input.GetButtonUp(input + playerNum);
+            case ButtonQuery.Down:
+                return Input.GetButtonDown(input + playerNum);
+            case ButtonQuery.Hold:
+                return Input.GetButton(input + playerNum);
+        }
+        return false;
+    }
 
 	public float getAxisX(ButtonQuery q)
 	{
 		string input = "JoystickX";
 
-		return Input.GetAxis (input + playerNum);
+        return Input.GetAxis (input + playerNum);
 	}
 
 	public float getAxisY(ButtonQuery q)
 	{
 		string input = "JoystickY";
-		return Input.GetAxis (input + playerNum);
+
+        return Input.GetAxis (input + playerNum);
 	}
 
 }
