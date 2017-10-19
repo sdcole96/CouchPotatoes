9ba76c0225ffc9df57bd1852e6ea86b22cd32121
Julien Fournell <fourn106@mail.chapman.edu>
2017-10-18 18:28:34 -0700

    Added ship models and upgraded target movement

--- Couch Potatoes/Assets/Our Assets/Scripts/Target.cs
+++ Couch Potatoes/Assets/Our Assets/Scripts/Target.cs
@@ -6,20 +6,37 @@ public class Target : MonoBehaviour
 {
 	// speed and angle
 	public float rotationSpeed = 1.0f;
-	public float rotationAngle = 90.0f;
+	public float rotationAngle = -90.0f;
+	public bool hit = false;
+
+	public GameObject pole;
+	public GameObject target;
+	public float originalY = 180f;
+	public float originalZ = 0f;
 
 
 	// Use this for initialization
 	void Start () 
 	{
-		
+		//originalY = target.transform.rotation.y;
+		//originalZ = target.transform.rotation.z;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.Translate (Vector3.left * Time.deltaTime);
-		Quaternion target = Quaternion.Euler (rotationAngle, 0, 0);
-		transform.rotation = Quaternion.Slerp (transform.rotation, target, Time.deltaTime * rotationSpeed);
+		pole.transform.Translate (Vector3.left * Time.deltaTime);
+		
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			hit = true;
+		}
+
+		if (hit) 
+		{
+			
+			Quaternion targetRotation = Quaternion.Euler (rotationAngle, originalY, originalZ);
+			target.transform.rotation = Quaternion.Slerp (target.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);	
+		}
 	}
 }
