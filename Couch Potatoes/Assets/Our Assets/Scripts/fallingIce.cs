bf2def86cfc9a4d4376448c738fd1d9539f30a22
Daniel <sdcole96@yahoo.com>
2017-10-23 16:55:34 -0700

    Added "Smart" Ice
    Ice now picks a random alive player, finds the closest block to it,
    then drops the ice.

--- Couch Potatoes/Assets/Our Assets/Scripts/fallingIce.cs
+++ Couch Potatoes/Assets/Our Assets/Scripts/fallingIce.cs
@@ -6,6 +6,8 @@ public class fallingIce : MonoBehaviour {
 
 	public int seconds = 5;
 	public int i = 0;
+	public GameObject[] players;
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (dropIce(seconds));
@@ -21,19 +23,44 @@ public class fallingIce : MonoBehaviour {
 	{
 		
 	}
+		
 
 	private IEnumerator dropIce(float waitTime)
 	{
 		int childCount = transform.childCount;
 		while (childCount > 2) 
 		{
+			childCount = transform.childCount;
+
 			yield return new WaitForSeconds (waitTime);
 
-			//childCount = transform.childCount;
-			int rand = Random.Range (0, transform.childCount);
-			Transform ice = transform.GetChild(rand).GetChild(0);
-			Animator iceAnim = ice.GetComponent<Animator> ();
+			players = GameObject.FindGameObjectsWithTag("Player");
+			int rand = Random.Range (0, players.Length);
+			GameObject g = findClosestIce (players[rand]);
+
+			//Transform ice = transform.GetChild(rand).GetChild(0);
+			Animator iceAnim = g.GetComponent<Animator> ();
 			iceAnim.Play ("FallingIce");
 		}
+	}
+
+	public GameObject findClosestIce(GameObject g)
+	{
+		GameObject[] ice;
+		ice = GameObject.FindGameObjectsWithTag ("Floor");
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = g.transform.position;
+		foreach (GameObject i in ice)
+		{
+			Vector3 diff = i.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if (curDistance < distance)
+			{
+				closest = i;
+				distance = curDistance;
+			}
+		}
+		return closest;
 	}
 }
