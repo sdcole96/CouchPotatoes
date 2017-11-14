7f8d8c9db11afc0e81f0e1c7a09f6ef6f315e1d9
Daniel <sdcole96@yahoo.com>
2017-11-13 19:32:15 -0800

    Made PowerUp Spawn Loop

--- Couch Potatoes/Assets/Our Assets/Scripts/Break The Ice/spawnPowers.cs
+++ Couch Potatoes/Assets/Our Assets/Scripts/Break The Ice/spawnPowers.cs
@@ -21,16 +21,16 @@ public class spawnPowers : MonoBehaviour {
 
 	IEnumerator spawnWait()
 	{
+		for (int j = 0; j < 10; j++) {
 			int i = Random.Range (5, 15);
-			yield return new WaitForSeconds(i);
+			yield return new WaitForSeconds (i);
 			GameObject[] g = GameObject.FindGameObjectsWithTag ("Floor");
 			GameObject ice = g [Random.Range (0, g.Length)];
 			GameObject power = Instantiate (powerUp, new Vector3 (ice.transform.position.x, 20, ice.transform.position.z), new Quaternion ());
 			power.AddComponent<Rigidbody> ();
 			power.AddComponent<BoxCollider> ();
 			power.tag = "Powerup";
-			switch (Random.Range (0, 2)) 
-			{
+			switch (Random.Range (0, 2)) {
 			case 0:
 				{
 					power.name = "Speed";
@@ -49,5 +49,6 @@ public class spawnPowers : MonoBehaviour {
 					break;
 				}
 			}
+		}
 	}
 }
