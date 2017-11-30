11347418ec2925c94f5e0364695cb069c8a9da0a
Julien Fournell <fourn106@mail.chapman.edu>
2017-11-29 20:10:25 -0800

    Seagull prototype added

---
+++ Couch Potatoes/Assets/PauseGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	public Transform canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			Pause ();
+		}
+	}
+
+	public void Quit()
+	{
+		Time.timeScale = 1;
+		ChangeScene cs = new ChangeScene ();
+		cs.LoadSceneMM ();
+	}
+
+	public void Pause()
+	{
+		if (canvas.gameObject.activeInHierarchy == false) 
+		{
+			canvas.gameObject.SetActive (true);
+			Time.timeScale = 0;
+		} 
+		else
+		{
+			canvas.gameObject.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+}
