4a8a752c5d7f163155748236179159f84ee7d71b
Fournell- Fuentes <fourn106@mail.chapman.edu>
2018-02-21 21:55:45 -0800

    Potatank shoots now

---
+++ The Real Couch Potatoes/Couch Potatoes/Assets/Our Assets/Scripts/Potatank/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(DestroyBullet());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    public IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ The Real Couch Potatoes/Couch Potatoes/Assets/Our Assets/Scripts/Potatank/TankController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+
+	private float rightStick;
+	private float leftStick;
+    public GameObject bullet;
+    public GameObject firingPoint;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		rightStick = GamePad.GetAxis (CAxis.RY);
+		leftStick = GamePad.GetAxis(CAxis.LY);
+		this.transform.Translate ((-Vector3.forward * rightStick - Vector3.forward * leftStick)*.3f);
+		this.transform.Rotate (Vector3.up, 2 * rightStick - 2 * leftStick);
+
+        if (GamePad.GetButton(CButton.RB) || GamePad.GetButton(PSButton.R1))
+        {
+            Instantiate(bullet, firingPoint.transform.position, firingPoint.transform.rotation);
+        }
+    }
+}
