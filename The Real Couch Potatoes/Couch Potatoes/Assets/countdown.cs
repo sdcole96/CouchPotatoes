53cc54c9e3258c26b40152b792a1f1e4ff98af50
Daniel <sdcole96@yahoo.com>
2018-02-22 13:07:57 -0800

    Added background stationary glacier

--- The Real Couch Potatoes/Couch Potatoes/Assets/countdown.cs
+++ The Real Couch Potatoes/Couch Potatoes/Assets/countdown.cs
@@ -5,31 +5,33 @@ using UnityEngine.UI;
 
 public class countdown : MonoBehaviour {
 
+	public float trig = 0;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		StartCoroutine (count ());
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
 	}
+
 
 	IEnumerator count()
 	{
 		int i = 4;
 		yield return new WaitForSeconds (5f);
-		float j = 0;
+		float j = 5;
 		while(i != 0)
 		{
-			j += .5f;
+			j += -j*2f;
 			yield return new WaitForSeconds (1f);
 			i--;
 
 			foreach (Transform child in transform)
 			{
 				child.GetComponent<Text> ().text = (i).ToString ();
-				child.GetComponent<RectTransform> ().position = child.GetComponent<RectTransform> ().position + new Vector3 (j, j, 0);
 			}
 		}
 		foreach (playerController p in GameObject.FindObjectsOfType<playerController> ()) 
