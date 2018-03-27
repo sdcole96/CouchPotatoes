561035fe8b8a86f4c7231b7bb365992fccc69606
Daniel <sdcole96@yahoo.com>
2018-03-27 11:52:05 -0700

    Wassup y'all i'm probably definitely breaking something by doing this

---
+++ The Real Couch Potatoes/Couch Potatoes/Assets/art1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class art1 : MonoBehaviour {
+
+	public float speed;
+	public int i = 0;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (lookgood (speed));	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator lookgood(float s)
+	{
+		i++;
+		yield return new WaitForSeconds (s);
+		this.transform.Rotate (new Vector3 (0, i, i));
+	}
+}
