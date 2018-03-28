0f909c472c43df8f1b1372d011c139b673589164
Daniel <sdcole96@yahoo.com>
2018-03-28 13:55:56 -0700

    TV fade fully functional on space bar

--- The Real Couch Potatoes/Couch Potatoes/Assets/Our Assets/Scripts/Essential/ChangeScene.cs
+++ The Real Couch Potatoes/Couch Potatoes/Assets/Our Assets/Scripts/Essential/ChangeScene.cs
@@ -72,16 +72,15 @@ public class ChangeScene : MonoBehaviour {
     IEnumerator LoadSceneAfterTransition()
     {
 		this.GetComponent<TVfade> ().work = true;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(2.5f);
 		GetComponent<fadeInFadeOut>().FadeIn();
         LoadSceneMM();
 
     }
     IEnumerator FadeThenStart(string sceneName)
     {
-		yield return new WaitForSeconds (3f);
+        yield return new WaitForSeconds(2.0f);
 		this.GetComponent<fadeInFadeOut>().FadeIn();
-        yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene(sceneName);
     }
 }
