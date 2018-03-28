85b7780f83e254580f1ecd811f58b355555a7b9c
Joshua Smith <joshner.smith@gmail.com>
2018-03-28 12:08:51 -0700

    Added Correct Color Schemes and Player Icons
    - Players now display correctly on minigame selection screen
    - Any player can move the "cursor" to select a minigame
    - Only entered players have their icons and score displayed on the selection screen

--- The Real Couch Potatoes/Couch Potatoes/Assets/Our Assets/Scripts/Essential/PlayerSelect.cs
+++ The Real Couch Potatoes/Couch Potatoes/Assets/Our Assets/Scripts/Essential/PlayerSelect.cs
@@ -10,8 +10,9 @@ public class PlayerSelect : MonoBehaviour
     public Text playerJoinText; // Player has joined the game textbox
     public Text startText; // Press start to begin textbox
     public Text[] pressATextBoxes;
-	public Image[] spriteImages; 
-	public ArrayList remainingSprites = new ArrayList();
+	public Image[] spriteImages;
+    private Color[] colorArray;
+    public ArrayList remainingSprites = new ArrayList();
 	public GameObject colorSelector;
 
     public bool playerJoined1 = true;
@@ -28,7 +29,12 @@ public class PlayerSelect : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-		
+        // Set your colors here;
+        colorArray = new Color[4];
+        colorArray[0] = Color.red;
+        colorArray[1] = Color.green;
+        colorArray[2] = Color.blue;
+        colorArray[3] = Color.yellow;
     }
 
     // Update is called once per frame
@@ -48,54 +54,58 @@ public class PlayerSelect : MonoBehaviour
 
         if (((GamePad.GetButton(CButton.A, PlayerIndex.One) || GamePad.GetButton(PSButton.Cross, PlayerIndex.One)) && GameMaster.activePlayers.Count < 4) && playerJoined1)
         {
-			spriteImages[GameMaster.activePlayers.Count].gameObject.SetActive(true);
-			pressATextBoxes[GameMaster.activePlayers.Count].enabled = false;
+            spriteImages[GameMaster.activePlayers.Count].gameObject.SetActive(true);
+            pressATextBoxes[GameMaster.activePlayers.Count].enabled = false;
             playerJoined1 = false;
             DropTater();
-			ColorSelect (PlayerIndex.One);
-			PlayerAudio ();
+            ColorSelect(PlayerIndex.One);
+            PlayerAudio();
             PlayerClass newPlayer = new PlayerClass(GameMaster.activePlayers.Count, PlayerIndex.One);
+            newPlayer.SetPColor(colorArray[GameMaster.activePlayers.Count]); // Set the color for the player, based on when they joined the game
             GameMaster.activePlayers.Add(newPlayer);
-			StartCoroutine(showPlayerJoined(GameMaster.activePlayers.Count, Color.red));
+            StartCoroutine(showPlayerJoined(GameMaster.activePlayers.Count, colorArray[GameMaster.activePlayers.Count - 1]));
             Debug.Log("Controller 1");
         }
         else if (((GamePad.GetButton(CButton.A, PlayerIndex.Two) || GamePad.GetButton(PSButton.Cross, PlayerIndex.Two)) && GameMaster.activePlayers.Count < 4) && playerJoined2)
         {
-			spriteImages[GameMaster.activePlayers.Count].gameObject.SetActive(true);
-			pressATextBoxes[GameMaster.activePlayers.Count].enabled = false;
+            spriteImages[GameMaster.activePlayers.Count].gameObject.SetActive(true);
+            pressATextBoxes[GameMaster.activePlayers.Count].enabled = false;
             playerJoined2 = false;
             DropTater();
-			ColorSelect (PlayerIndex.Two);
-			PlayerAudio ();
+            ColorSelect(PlayerIndex.Two);
+            PlayerAudio();
             PlayerClass newPlayer = new PlayerClass(GameMaster.activePlayers.Count, PlayerIndex.Two);
+            newPlayer.SetPColor(colorArray[GameMaster.activePlayers.Count]); // Set the color for the player, based on when they joined the game
             GameMaster.activePlayers.Add(newPlayer);
-			StartCoroutine(showPlayerJoined(GameMaster.activePlayers.Count, Color.green));
+            StartCoroutine(showPlayerJoined(GameMaster.activePlayers.Count, colorArray[GameMaster.activePlayers.Count - 1]));
             Debug.Log("Controller 2");
         }
         else if (((GamePad.GetButton(CButton.A, PlayerIndex.Three) || GamePad.GetButton(PSButton.Cross, PlayerIndex.Three)) && GameMaster.activePlayers.Count < 4) && playerJoined3)
         {
-			spriteImages[GameMaster.activePlayers.Count].gameObject.SetActive(true);
-			pressATextBoxes[GameMaster.activePlayers.Count].enabled = false;
+            spriteImages[GameMaster.activePlayers.Count].gameObject.SetActive(true);
+            pressATextBoxes[GameMaster.activePlayers.Count].enabled = false;
             playerJoined3 = false;
             DropTater();
-			ColorSelect (PlayerIndex.Three);
-			PlayerAudio ();
+            ColorSelect(PlayerIndex.Three);
+            PlayerAudio();
             PlayerClass newPlayer = new PlayerClass(GameMaster.activePlayers.Count, PlayerIndex.Three);
+            newPlayer.SetPColor(colorArray[GameMaster.activePlayers.Count]); // Set the color for the player, based on when they joined the game
             GameMaster.activePlayers.Add(newPlayer);
-			StartCoroutine(showPlayerJoined(GameMaster.activePlayers.Count, Color.blue));
+            StartCoroutine(showPlayerJoined(GameMaster.activePlayers.Count, colorArray[GameMaster.activePlayers.Count - 1]));
             Debug.Log("Controller 3");
         }
         else if (((GamePad.GetButton(CButton.A, PlayerIndex.Four) || GamePad.GetButton(PSButton.Cross, PlayerIndex.Four)) && GameMaster.activePlayers.Count < 4) && playerJoined4)
         {
-			spriteImages[GameMaster.activePlayers.Count].gameObject.SetActive(true);
-			pressATextBoxes[GameMaster.activePlayers.Count].enabled = false;
+            spriteImages[GameMaster.activePlayers.Count].gameObject.SetActive(true);
+            pressATextBoxes[GameMaster.activePlayers.Count].enabled = false;
             playerJoined4 = false;
             DropTater();
-			ColorSelect (PlayerIndex.Four);
-			PlayerAudio ();
+            ColorSelect(PlayerIndex.Four);
+            PlayerAudio();
             PlayerClass newPlayer = new PlayerClass(GameMaster.activePlayers.Count, PlayerIndex.Four);
+            newPlayer.SetPColor(colorArray[GameMaster.activePlayers.Count]); // Set the color for the player, based on when they joined the game
             GameMaster.activePlayers.Add(newPlayer);
-			StartCoroutine(showPlayerJoined(GameMaster.activePlayers.Count, Color.yellow));
+            StartCoroutine(showPlayerJoined(GameMaster.activePlayers.Count, colorArray[GameMaster.activePlayers.Count - 1]));
             Debug.Log("Controller 4");
         }
         /*
---
+++ The Real Couch Potatoes/Couch Potatoes/Assets/Our Assets/Scripts/displayPlayers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayPlayers : MonoBehaviour {
+
+    public GameObject[] spriteBoxes;
+    public RuntimeAnimatorController redAnim;
+    public RuntimeAnimatorController greenAnim;
+    public RuntimeAnimatorController blueAnim;
+    public RuntimeAnimatorController yellowAnim;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        for (int i = 0; i < GameMaster.activePlayers.Count; i++)
+        {
+            Color colorToCheck = GameMaster.activePlayers[i].GetPColor();
+            spriteBoxes[i].GetComponent<Image>().enabled = true; // Enable Image
+            if (colorToCheck == Color.red)
+                spriteBoxes[i].GetComponent<Animator>().runtimeAnimatorController = redAnim as RuntimeAnimatorController; // Pop in requested animation
+            if (colorToCheck == Color.green)
+                spriteBoxes[i].GetComponent<Animator>().runtimeAnimatorController = greenAnim as RuntimeAnimatorController; // Pop in requested animation
+            if (colorToCheck == Color.blue)
+                spriteBoxes[i].GetComponent<Animator>().runtimeAnimatorController = blueAnim as RuntimeAnimatorController; // Pop in requested animation
+            if (colorToCheck == Color.yellow)
+                spriteBoxes[i].GetComponent<Animator>().runtimeAnimatorController = yellowAnim as RuntimeAnimatorController; // Pop in requested animation
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
