c25fc6656d42a61dc8a28a860cd745497cf90390
Joshua Smith <joshner.smith@gmail.com>
2018-03-31 22:59:32 -0700

    Added Hovering UI over Players For Break the Ice
    - UI Now is attached to the Potato Player model (not to tanks yet)
    - World Space UI is disabled manually via the inspector for the main menu

---
+++ The Real Couch Potatoes/Couch Potatoes/Assets/Our Assets/Scripts/Essential/PlayerOverheadUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerOverheadUI : MonoBehaviour {
+
+
+    playerController pc;
+    Text textComponent;
+    
+
+	// Use this for initialization
+	void Start () {
+        pc = this.gameObject.GetComponent<playerController>();
+        textComponent = this.gameObject.GetComponentInChildren<Text>();
+        textComponent.text = "P" + pc.playerNum.ToString();
+        textComponent.color = GameMaster.activePlayers[pc.playerNum - 1].GetPColor();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Hide UI text when game begins
+        if (textComponent.enabled && pc.enabled)
+            textComponent.enabled = false;
+	}
+}
